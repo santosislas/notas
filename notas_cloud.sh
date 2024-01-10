e3374a3c64626f08bc122cace351222eb43a549d
Alberto Santos <alberto.santos@nielseniq.com>
2024-01-09 18:52:42 -0600

    add new notes

--- notas_cloud.sh
+++ notas_cloud.sh
@@ -1,24 +1,23 @@
-#antes de iniciar una aplicacion debemos crear un resource group
-#para hacer eso hacemos lo siguiente
-#nos logueamos a azure
+High_Availability. La capacidad de varias maquinas virtuales para activarse automaticamente y ayudar a procesar las solicitudes
+					cuando sea necesario
 
-az login
+Realibility(Confiabilidad) . garantiza que no tenga tiempo de inactividad para los servicios proporcionados por azure y
+							tambien proporciona un mecanismo automaticamente de recuperarse tras una falla
 
-#lo anterior nos abrira el browser por default y nos firmara en el portal de azure
-#vamos a crear el resource group para nuestra aplicacion
+Scability . la capacidad de agregar o quitar vm's escalando horizontalmente , así como agregar o quitar recursos de cada una
+			de nuestras maquinas virtuales'
 
-az group create --name spring_cloud_azure --location eastus
+Predictability. 'se refiere a saber que su aplicacion siempre funcionara como se espera'
 
-#Azure keu vault es un servicio que nos proporciona seguridad y confianza para almacenar y guardar
-#informacion sensible
-#para crear un key vault hacemos lo siguiente
+Security. 'Tener control total como se desee sobre su entorno de nube'
 
-az keyvault create --name "new_keyvault" --resource-group "spring_cloud_azure" --location "EastUS"
 
-#una vez creado el key vault podemos guardar informacion sensible
-#de la siguiente manera
-	
+Infrastructure_as_a_service (Iaas)
 
-#borrar un resource group
+Platform_as_a_service (Paas)
 
-az group delete --name "spring_cloud_azure"
+Software_as_a_services (Saas)
+
+Regions_And_Availability_Zones
+'la cantidad de datacenters fisicos que hay en una region una de otro , es decir , si desde un datacenter enviamos un flujo
+de datos a otro data center y este tarda 10 ms en llegar , la latencia es de 10 ms'
